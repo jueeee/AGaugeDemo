1bf2f7ebb6000485cf3aeb49543aaf612d114f1a
zhenhua Liu <qiyubrother@outlook.com>
2019-08-27 10:07:31 +0800

    添加项目文件。

---
+++ ComTest/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ComTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aGauge1 = new System.Windows.Forms.AGauge();
+            this.aGauge2 = new System.Windows.Forms.AGauge();
+            this.aGauge4 = new System.Windows.Forms.AGauge();
+            this.SuspendLayout();
+            // 
+            // aGauge1
+            // 
+            this.aGauge1.BaseArcColor = System.Drawing.Color.Gray;
+            this.aGauge1.BaseArcRadius = 80;
+            this.aGauge1.BaseArcStart = 135;
+            this.aGauge1.BaseArcSweep = 270;
+            this.aGauge1.BaseArcWidth = 2;
+            this.aGauge1.Center = new System.Drawing.Point(100, 100);
+            this.aGauge1.Location = new System.Drawing.Point(12, 12);
+            this.aGauge1.MaxValue = 400F;
+            this.aGauge1.MinValue = -100F;
+            this.aGauge1.Name = "aGauge1";
+            this.aGauge1.NeedleColor1 = System.Windows.Forms.AGaugeNeedleColor.Gray;
+            this.aGauge1.NeedleColor2 = System.Drawing.Color.DimGray;
+            this.aGauge1.NeedleRadius = 80;
+            this.aGauge1.NeedleType = System.Windows.Forms.NeedleType.Advance;
+            this.aGauge1.NeedleWidth = 2;
+            this.aGauge1.ScaleLinesInterColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesInterInnerRadius = 73;
+            this.aGauge1.ScaleLinesInterOuterRadius = 80;
+            this.aGauge1.ScaleLinesInterWidth = 1;
+            this.aGauge1.ScaleLinesMajorColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesMajorInnerRadius = 70;
+            this.aGauge1.ScaleLinesMajorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMajorStepValue = 50F;
+            this.aGauge1.ScaleLinesMajorWidth = 2;
+            this.aGauge1.ScaleLinesMinorColor = System.Drawing.Color.Green;
+            this.aGauge1.ScaleLinesMinorInnerRadius = 75;
+            this.aGauge1.ScaleLinesMinorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMinorTicks = 9;
+            this.aGauge1.ScaleLinesMinorWidth = 1;
+            this.aGauge1.ScaleNumbersColor = System.Drawing.Color.Chartreuse;
+            this.aGauge1.ScaleNumbersFormat = null;
+            this.aGauge1.ScaleNumbersRadius = 95;
+            this.aGauge1.ScaleNumbersRotation = 0;
+            this.aGauge1.ScaleNumbersStartScaleLine = 0;
+            this.aGauge1.ScaleNumbersStepScaleLines = 1;
+            this.aGauge1.Size = new System.Drawing.Size(205, 180);
+            this.aGauge1.TabIndex = 0;
+            this.aGauge1.Text = "aGauge1";
+            this.aGauge1.Value = 0F;
+            // 
+            // aGauge2
+            // 
+            this.aGauge2.BaseArcColor = System.Drawing.Color.Gray;
+            this.aGauge2.BaseArcRadius = 80;
+            this.aGauge2.BaseArcStart = 135;
+            this.aGauge2.BaseArcSweep = 270;
+            this.aGauge2.BaseArcWidth = 2;
+            this.aGauge2.Center = new System.Drawing.Point(100, 100);
+            this.aGauge2.Location = new System.Drawing.Point(298, 12);
+            this.aGauge2.MaxValue = 400F;
+            this.aGauge2.MinValue = -100F;
+            this.aGauge2.Name = "aGauge2";
+            this.aGauge2.NeedleColor1 = System.Windows.Forms.AGaugeNeedleColor.Gray;
+            this.aGauge2.NeedleColor2 = System.Drawing.Color.DimGray;
+            this.aGauge2.NeedleRadius = 80;
+            this.aGauge2.NeedleType = System.Windows.Forms.NeedleType.Advance;
+            this.aGauge2.NeedleWidth = 2;
+            this.aGauge2.ScaleLinesInterColor = System.Drawing.Color.Black;
+            this.aGauge2.ScaleLinesInterInnerRadius = 73;
+            this.aGauge2.ScaleLinesInterOuterRadius = 80;
+            this.aGauge2.ScaleLinesInterWidth = 1;
+            this.aGauge2.ScaleLinesMajorColor = System.Drawing.Color.Black;
+            this.aGauge2.ScaleLinesMajorInnerRadius = 70;
+            this.aGauge2.ScaleLinesMajorOuterRadius = 80;
+            this.aGauge2.ScaleLinesMajorStepValue = 50F;
+            this.aGauge2.ScaleLinesMajorWidth = 2;
+            this.aGauge2.ScaleLinesMinorColor = System.Drawing.Color.Gray;
+            this.aGauge2.ScaleLinesMinorInnerRadius = 75;
+            this.aGauge2.ScaleLinesMinorOuterRadius = 80;
+            this.aGauge2.ScaleLinesMinorTicks = 9;
+            this.aGauge2.ScaleLinesMinorWidth = 1;
+            this.aGauge2.ScaleNumbersColor = System.Drawing.Color.Black;
+            this.aGauge2.ScaleNumbersFormat = null;
+            this.aGauge2.ScaleNumbersRadius = 95;
+            this.aGauge2.ScaleNumbersRotation = 0;
+            this.aGauge2.ScaleNumbersStartScaleLine = 0;
+            this.aGauge2.ScaleNumbersStepScaleLines = 1;
+            this.aGauge2.Size = new System.Drawing.Size(205, 180);
+            this.aGauge2.TabIndex = 1;
+            this.aGauge2.Text = "aGauge2";
+            this.aGauge2.Value = 0F;
+            // 
+            // aGauge4
+            // 
+            this.aGauge4.BaseArcColor = System.Drawing.Color.Gray;
+            this.aGauge4.BaseArcRadius = 80;
+            this.aGauge4.BaseArcStart = 180;
+            this.aGauge4.BaseArcSweep = 180;
+            this.aGauge4.BaseArcWidth = 2;
+            this.aGauge4.Center = new System.Drawing.Point(100, 100);
+            this.aGauge4.Location = new System.Drawing.Point(570, 12);
+            this.aGauge4.MaxValue = 300F;
+            this.aGauge4.MinValue = 0F;
+            this.aGauge4.Name = "aGauge4";
+            this.aGauge4.NeedleColor1 = System.Windows.Forms.AGaugeNeedleColor.Gray;
+            this.aGauge4.NeedleColor2 = System.Drawing.Color.DimGray;
+            this.aGauge4.NeedleRadius = 80;
+            this.aGauge4.NeedleType = System.Windows.Forms.NeedleType.Advance;
+            this.aGauge4.NeedleWidth = 2;
+            this.aGauge4.ScaleLinesInterColor = System.Drawing.Color.Black;
+            this.aGauge4.ScaleLinesInterInnerRadius = 73;
+            this.aGauge4.ScaleLinesInterOuterRadius = 80;
+            this.aGauge4.ScaleLinesInterWidth = 1;
+            this.aGauge4.ScaleLinesMajorColor = System.Drawing.Color.DarkRed;
+            this.aGauge4.ScaleLinesMajorInnerRadius = 70;
+            this.aGauge4.ScaleLinesMajorOuterRadius = 80;
+            this.aGauge4.ScaleLinesMajorStepValue = 50F;
+            this.aGauge4.ScaleLinesMajorWidth = 2;
+            this.aGauge4.ScaleLinesMinorColor = System.Drawing.Color.Gray;
+            this.aGauge4.ScaleLinesMinorInnerRadius = 75;
+            this.aGauge4.ScaleLinesMinorOuterRadius = 80;
+            this.aGauge4.ScaleLinesMinorTicks = 9;
+            this.aGauge4.ScaleLinesMinorWidth = 1;
+            this.aGauge4.ScaleNumbersColor = System.Drawing.Color.Black;
+            this.aGauge4.ScaleNumbersFormat = null;
+            this.aGauge4.ScaleNumbersRadius = 95;
+            this.aGauge4.ScaleNumbersRotation = 0;
+            this.aGauge4.ScaleNumbersStartScaleLine = 0;
+            this.aGauge4.ScaleNumbersStepScaleLines = 1;
+            this.aGauge4.Size = new System.Drawing.Size(205, 114);
+            this.aGauge4.TabIndex = 3;
+            this.aGauge4.Text = "aGauge4";
+            this.aGauge4.Value = 0F;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aGauge4);
+            this.Controls.Add(this.aGauge2);
+            this.Controls.Add(this.aGauge1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.AGauge aGauge1;
+        private System.Windows.Forms.AGauge aGauge2;
+        private System.Windows.Forms.AGauge aGauge4;
+    }
+}
